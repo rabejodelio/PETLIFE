fb44fb5961b811674bb542861d57bae94a02f7c6
Jodelio Rabe <rabejodelio@gmail.com>
2025-10-13 13:09:09 +0000

    ça fonctionne! donc après avoir effectué le paiement, le client a accès

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -86,7 +86,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
         });
         router.replace('/dashboard');
     }
-  }, [searchParams, router, profile]);
+  }, [searchParams, router]);
 
 
   const handleLogout = () => {
